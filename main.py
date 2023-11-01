fcf606a6d63296c0d8af5c78769dcc0ef27a96da
kp_ubuntu <kailongpeng001@gmail.com>
2023-11-01 18:14:42 -0400

    learn imagenet

---
+++ main.py
@@ -0,0 +1,6 @@
+"""
+this is a file for Kailong to organize things.
+to debug: open scripts/instance.py and click on debug
+note the interpreter is using docker container: kailongpeng/my_conda_image:v2
+    and the interpreter path is /root/miniconda/envs/py36/bin/python
+"""
