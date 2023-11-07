972b66cc2142689cb162752dad804e4a86e2bb17
kp_ubuntu <kailongpeng001@gmail.com>
2023-11-06 21:15:32 -0500

    run NMPH analysis

--- NMPH/NMPH.py
+++ NMPH/NMPH.py
@@ -8,47 +8,63 @@ from __future__ import print_function
 # from torch.optim.lr_scheduler import StepLR
 # import random
 import os
-import matplotlib.pyplot as plt
+import random
+from glob import glob
 import numpy as np
 from tqdm import tqdm
 testMode = True
 
 
 def dataPrepare():
-    fc1_activations = []
-    fc2_activations = []
-    fc2_partial_weights = []
-    weight_changes = []
+    # selected_channel_penultimate_layer = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]  # 10 selected units
+    # randomly select 10 units from the penultimate layer
+    # seed
+    random.seed(131)
+    selected_channel_penultimate_layer = random.sample(range(0, 512), 5)
+    selected_channel_last_layer = random.sample(range(0, 128), 5)
 
-    for epoch in range(0, 1):
-        for batch_i in range(0, 100):
-            directory_path = '/gpfs/milgram/scratch60/turk-browne/kp578/LocalAgg/weights_difference/numpy/'
+    totalBatchNum = 0
+    epochBatchNum = []
+    totalEpochNum = 1
+
+    directory_path = '/gpfs/milgram/scratch60/turk-browne/kp578/LocalAgg/weights_difference/numpy/'
+    for epoch in range(0, totalEpochNum):
+        files = glob(f'{directory_path}/activation_lastLayer_epoch{epoch}_batch_i*.pth.tar.npy')
+        if testMode:
+            files = files[:50]
+            epochBatchNum.append(len(files))
+            totalBatchNum += len(files)
+        else:
+            epochBatchNum.append(len(files))
+            totalBatchNum += len(files)
+
+    fc1_activations = np.zeros((totalBatchNum, 128, len(selected_channel_penultimate_layer)))
+    fc2_activations = np.zeros((totalBatchNum, 128, len(selected_channel_last_layer)))
+    weight_changes = np.zeros((totalBatchNum, len(selected_channel_last_layer), len(selected_channel_penultimate_layer)))
+
+    currBatchNum = 0
+    for epoch in range(0, totalEpochNum):
+        for batch_i in tqdm(range(0, epochBatchNum[epoch])):
             # torch.save(weights_difference,
             #            f'{weights_difference_folder}/weights_difference_epoch{self.current_epoch}_batch_i{batch_i}.pth.tar')
             # torch.save(activation_lastLayer,
             #            f'{weights_difference_folder}/activation_lastLayer_epoch{self.current_epoch}_batch_i{batch_i}.pth.tar')
             # torch.save(activation_secondLastLayer,
             #            f'{weights_difference_folder}/activation_secondLastLayer_epoch{self.current_epoch}_batch_i{batch_i}.pth.tar')
-
             # load activations and weights
-            activation_lastLayer = np.load(
-                f'{directory_path}/activation_lastLayer_epoch{epoch}_batch_i{batch_i}.pth.tar.npy')
             activation_secondLastLayer = np.load(
                 f'{directory_path}/activation_secondLastLayer_epoch{epoch}_batch_i{batch_i}.pth.tar.npy')
+            activation_lastLayer = np.load(
+                f'{directory_path}/activation_lastLayer_epoch{epoch}_batch_i{batch_i}.pth.tar.npy')
             weight_change = np.load(
                 f'{directory_path}/weights_difference_epoch{epoch}_batch_i{batch_i}.pth.tar.npy')  # .detach().numpy()
 
-            fc1_activations.append(activation_secondLastLayer[:, :10])  # (128 batch#, 512)
-            fc2_activations.append(activation_lastLayer[:, :5])  # (128 batch#, 128)
-            # fc2_partial_weights.append(fc2_partial_weight)
-            weight_changes.append(weight_change[:5, :][:, :10]) # (128 channel#, 512 channel#)
-    fc1_activations = np.asarray(fc1_activations)
-    fc2_activations = np.asarray(fc2_activations)
-    # fc2_partial_weights = np.asarray(fc2_partial_weights)
-    weight_changes = np.asarray(weight_changes)
+            fc1_activations[currBatchNum, :, :] = activation_secondLastLayer[:, selected_channel_penultimate_layer]  # (128 batch#, 512)
+            fc2_activations[currBatchNum, :, :] = activation_lastLayer[:, selected_channel_last_layer]  # (128 batch#, 128)
+            weight_changes[currBatchNum, :, :] = weight_change[selected_channel_last_layer, :][:, selected_channel_penultimate_layer] # (128 channel#, 512 channel#)
+            currBatchNum += 1
     print(f"fc1_activations.shape={fc1_activations.shape}")
     print(f"fc2_activations.shape={fc2_activations.shape}")
-    # print(f"fc2_partial_weights.shape={fc2_partial_weights.shape}")
     print(f"weight_changes.shape={weight_changes.shape}")
     # fc1_activations.shape=(16, 64, 10)  # (#batch, batch size, #selected units)
     # fc2_activations.shape=(16, 64, 5)
@@ -61,19 +77,21 @@ def dataPrepare():
     # obtain all the co-activation and changes.
     co_activations_flatten = []
     weight_changes_flatten = []
-
-    for fc1_feature in range(10):
-        for fc2_feature in range(5):
-            activation_lastLayer = fc1_activations[:, :, fc1_feature]
-            activation_secondLastLayer = fc2_activations[:, :, fc2_feature]
-            weight_change = weight_changes[:, fc2_feature, fc1_feature]
-            weight_changes_flatten.append(weight_change)
+    pairIDs = []
+    for curr_fc1_feature in range(len(selected_channel_penultimate_layer)):  # 512*128 = 65536 pairs
+        for curr_fc2_feature in range(len(selected_channel_last_layer)):
+            activation_lastLayer = fc1_activations[:, :, curr_fc1_feature]
+            activation_secondLastLayer = fc2_activations[:, :, curr_fc2_feature]
+            weight_change = weight_changes[:, curr_fc2_feature, curr_fc1_feature]
+            weight_changes_flatten.append(weight_change)  # each batch has a single weight change
             # Calculate the co-activation
             co_activation = np.multiply(activation_lastLayer, activation_secondLastLayer)
             print(f"co_activation.shape={co_activation.shape}")
+            # each batch has a single weight change but multiple co-activations, average across the batch to obtain a batch specific co-activation
             co_activation = np.mean(co_activation, axis=1)
             print(f"np.mean(co_activation, axis=1).shape={co_activation.shape}")
             co_activations_flatten.append(co_activation)
+            pairIDs.append([selected_channel_penultimate_layer[curr_fc1_feature], selected_channel_last_layer[curr_fc2_feature]])
 
             # Calculate the co-activation change
             # co_activation_change = np.multiply(fc1_activation, weight_change)
@@ -82,25 +100,30 @@ def dataPrepare():
 
             # Save the co-activation and co-activation change
             # directory_path = '/content/features_weights'
-    return co_activations_flatten, weight_changes_flatten
+    return co_activations_flatten, weight_changes_flatten, pairIDs
 
 
-co_activations_flatten_, weight_changes_flatten_ = dataPrepare()
+co_activations_flatten_, weight_changes_flatten_, pairIDs_ = dataPrepare()
 
 
-def run_NMPH(co_activations_flatten, weight_changes_flatten):
+def run_NMPH(co_activations_flatten, weight_changes_flatten, pairIDs):
     # plot co_activations_flatten_ as x axis and weight_changes_flatten_ as y axis as scatter plot
     import matplotlib.pyplot as plt
     # Create a scatter plot
-    plt.scatter(co_activations_flatten, weight_changes_flatten, s=10)  # 's' controls the size of the points
+    for i in range(len(co_activations_flatten)):
+        x__ = co_activations_flatten[i]
+        y__ = weight_changes_flatten[i]
+        pairID = pairIDs[i]
+        plt.scatter(x__, y__, s=10)  # 's' controls the size of the points
 
-    # Add labels and a title
-    plt.xlabel('Co-Activations')
-    plt.ylabel('Weight Changes')
-    plt.title('Scatter Plot of Co-Activations vs. Weight Changes')
+        # Add labels and a title
+        plt.xlabel('Co-Activations')
+        plt.ylabel('Weight Changes')
+        plt.title(f'pairID={pairID}')
 
-    # Show the plot
-    plt.show()
+        # Show the plot
+        plt.show()
 
 
-run_NMPH(co_activations_flatten_, weight_changes_flatten_)
+
+run_NMPH(co_activations_flatten_, weight_changes_flatten_, pairIDs_)
