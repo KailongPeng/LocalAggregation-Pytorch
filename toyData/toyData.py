3a2ca4caae63b2b55b44fcbcb1bc297a5e7c6da9
KailongPeng <kailongpeng001@gmail.com>
2023-12-13 03:19:23 -0500

    .

--- toyData/toyData.py
+++ toyData/toyData.py
@@ -1,3 +1,5 @@
+import os
+
 import numpy as np
 import torch
 import torch.nn as nn
@@ -180,7 +182,7 @@ def trainWith_crossEntropyLoss():
     plt.show()
 
 
-def test_multiple_dotsNeighbotSIngleBatch(threeD_input=None):
+def test_multiple_dotsNeighbotSingleBatch(threeD_input=None):
     import torch
     import torch.nn as nn
     import torch.optim as optim
@@ -302,6 +304,11 @@ def test_multiple_dotsNeighbotSIngleBatch(threeD_input=None):
 
     total_epochs = 1000
 
+    # Record the weights, penultimate layer activations, and final layer activations
+    weight_difference_history = {'input_layer': [], 'hidden_layer1': [], 'hidden_layer2': []}
+    activation_history = {'penultimate_layer_before': [], 'final_layer_before': [],
+                          'penultimate_layer_after': [], 'final_layer_after': []}
+
     for epoch in tqdm(range(total_epochs)):
         if epoch == int(total_epochs / 3):
             optimizer.param_groups[0]['lr'] = learning_rate / 2.0
@@ -312,6 +319,18 @@ def test_multiple_dotsNeighbotSIngleBatch(threeD_input=None):
         epoch_loss = 0.0  # Variable to accumulate loss within each epoch
 
         for curr_batch, (batch, batch_labels) in enumerate(dataloader):
+            # Record weights
+            input_layer_before = net.input_layer.weight.data.clone().detach().numpy()
+            hidden_layer1_before = net.hidden_layer1.weight.data.clone().detach().numpy()
+            hidden_layer2_before = net.hidden_layer2.weight.data.clone().detach().numpy()
+
+            # Record activations
+            penultimate_activations = torch.relu(net.hidden_layer1(torch.relu(net.input_layer(batch.float()))))
+            final_activations = net.hidden_layer2(penultimate_activations)
+
+            activation_history['penultimate_layer_before'].append(penultimate_activations.detach().numpy())
+            activation_history['final_layer_before'].append(final_activations.detach().numpy())
+
             # Zero gradients
             optimizer.zero_grad()
             # Forward pass
@@ -333,6 +352,22 @@ def test_multiple_dotsNeighbotSIngleBatch(threeD_input=None):
             optimizer.step()
 
             epoch_loss += loss.item()
+
+            # Record weights
+            input_layer_after = net.input_layer.weight.data.clone().detach().numpy()
+            hidden_layer1_after = net.hidden_layer1.weight.data.clone().detach().numpy()
+            hidden_layer2_after = net.hidden_layer2.weight.data.clone().detach().numpy()
+
+            weight_difference_history['input_layer'].append(input_layer_after - input_layer_before)
+            weight_difference_history['hidden_layer1'].append(hidden_layer1_after - hidden_layer1_before)
+            weight_difference_history['hidden_layer2'].append(hidden_layer2_after - hidden_layer2_before)
+
+            # Record activations
+            penultimate_activations = torch.relu(net.hidden_layer1(torch.relu(net.input_layer(batch.float()))))
+            final_activations = net.hidden_layer2(penultimate_activations)
+
+            activation_history['penultimate_layer_after'].append(penultimate_activations.detach().numpy())
+            activation_history['final_layer_after'].append(final_activations.detach().numpy())
 
             # record initial and final latent space points
             if epoch == total_epochs - 1:
@@ -383,8 +418,21 @@ def test_multiple_dotsNeighbotSIngleBatch(threeD_input=None):
     plt.tight_layout()
     plt.show()
 
+    return weight_difference_history, activation_history
 
-test_multiple_dotsNeighbotSIngleBatch(threeD_input=False)
+
+weight_difference_history, activation_history = test_multiple_dotsNeighbotSingleBatch(threeD_input=False)
+
+weight_difference_folder = f"/gpfs/milgram/scratch60/turk-browne/kp578/LocalAgg/toyData/weight_difference_folder/"
+os.makedirs(weight_difference_folder, exist_ok=True)
+np.save(f'{weight_difference_folder}/weight_difference_history_input_layer.npy', np.asarray(weight_difference_history['input_layer']))  # (20000, 64, 2)  1000points/50imagesPerBatch=20batchPerEpoch, in total there are 20*1000epoch=20000 batches
+np.save(f'{weight_difference_folder}/weight_difference_history_hidden_layer1.npy', np.asarray(weight_difference_history['hidden_layer1']))  # (20000, 32, 64)
+np.save(f'{weight_difference_folder}/weight_difference_history_hidden_layer2.npy', np.asarray(weight_difference_history['hidden_layer2']))  # (20000, 2, 32)
+
+np.save(f'{weight_difference_folder}/activation_history_penultimate_layer_before.npy', np.asarray(activation_history['penultimate_layer_before']))  # (20000, 50, 32)
+np.save(f'{weight_difference_folder}/activation_history_final_layer_before.npy', np.asarray(activation_history['final_layer_before']))  # (20000, 50, 2)
+np.save(f'{weight_difference_folder}/activation_history_penultimate_layer_after.npy', np.asarray(activation_history['penultimate_layer_after']))  # (20000, 50, 32)
+np.save(f'{weight_difference_folder}/activation_history_final_layer_after.npy', np.asarray(activation_history['final_layer_after']))  # (20000, 50, 2)
 
 """
     representational level
@@ -400,4 +448,276 @@ test_multiple_dotsNeighbotSIngleBatch(threeD_input=False)
         
         plot the weight difference against the co-activation as X and Y axes respectively. This should be the synaptic level NMPH curve.  
 """
+
+
+def synaptic_level():
+    import random
+    from scipy.stats import pearsonr
+    from scipy.optimize import curve_fit
+    testMode = False
+    directory_path = f"/gpfs/milgram/scratch60/turk-browne/kp578/LocalAgg/toyData/synaptic_level/"
+    os.makedirs(directory_path, exist_ok=True)
+    def dataPrepare():
+        # seed
+        random.seed(131)
+        selected_channel_penultimate_layer = random.sample(range(0, 32), 32)
+        selected_channel_last_layer = random.sample(range(0, 2), 2)
+        selected_channel_penultimate_layer.sort()
+        selected_channel_last_layer.sort()
+
+        # epochBatchNum = {}
+        # startFromEpoch = 0
+        # totalEpochNum = 1
+        weight_difference_folder = f"/gpfs/milgram/scratch60/turk-browne/kp578/LocalAgg/toyData/weight_difference_folder/"
+        weight_difference_history_input_layer = np.load(f'{weight_difference_folder}/weight_difference_history_input_layer.npy')
+
+        totalBatchNum = weight_difference_history_input_layer.shape[0]
+        print(f"totalBatchNum={totalBatchNum}")
+        # layerA_activations = np.zeros(
+        #     (totalBatchNum, 50, len(selected_channel_penultimate_layer)))  # [#batch, batch size, #selected units]
+        # layerB_activations = np.zeros(
+        #     (totalBatchNum, 50, len(selected_channel_last_layer)))
+        # weight_changes = np.zeros(
+        #     (totalBatchNum, len(selected_channel_last_layer), len(selected_channel_penultimate_layer)))
+
+        layerA_activations = np.load(f'{weight_difference_folder}/activation_history_penultimate_layer_before.npy')
+        layerB_activations = np.load(f'{weight_difference_folder}/activation_history_final_layer_before.npy')
+        weight_changes = np.load(f'{weight_difference_folder}/weight_difference_history_hidden_layer2.npy')
+
+        # currBatchNum = 0
+        # # for epoch in range(startFromEpoch, totalEpochNum):
+        # for batch_i in tqdm(range(0, epochBatchNum[epoch])):
+        #     # load activations and weights
+        #     activation_secondLastLayer = np.load(
+        #         f'{directory_path}/activation_secondLastLayer_epoch{epoch}_batch_i{batch_i}.npy')
+        #     activation_lastLayer = np.load(
+        #         f'{directory_path}/activation_lastLayer_epoch{epoch}_batch_i{batch_i}.npy')
+        #     weight_change = np.load(
+        #         f'{directory_path}/weights_difference_epoch{epoch}_batch_i{batch_i}.npy')  # .detach().numpy()
+        #
+        #     layerA_activations[currBatchNum, :, :] = activation_secondLastLayer[:,
+        #                                           selected_channel_penultimate_layer]  # (128 batch#, 512)
+        #     layerB_activations[currBatchNum, :, :] = activation_lastLayer[:,
+        #                                           selected_channel_last_layer]  # (128 batch#, 128)
+        #     weight_changes[currBatchNum, :, :] = weight_change[selected_channel_last_layer, :][:,
+        #                                          selected_channel_penultimate_layer]  # (128 channel#, 512 channel#)
+        #     currBatchNum += 1
+        # print(f"layerA_activations.shape={layerA_activations.shape}")
+        # print(f"layerB_activations.shape={layerB_activations.shape}")
+        # print(f"weight_changes.shape={weight_changes.shape}")
+
+        # obtain all the co-activation and changes.
+        co_activations_flatten = []
+        weight_changes_flatten = []
+        pairIDs = []
+        for curr_fc1_feature in tqdm(range(len(selected_channel_penultimate_layer))):  # 512*128 = 65536 pairs
+            for curr_fc2_feature in range(len(selected_channel_last_layer)):
+                activation_lastLayer = layerA_activations[:, :, curr_fc1_feature]  # [#batch, batch size， channel#]
+                activation_secondLastLayer = layerB_activations[:, :, curr_fc2_feature]
+                weight_change = weight_changes[:, curr_fc2_feature, curr_fc1_feature]
+                weight_changes_flatten.append(weight_change)  # each batch has a single weight change
+                # Calculate the co-activation
+                co_activation = np.multiply(activation_lastLayer, activation_secondLastLayer)
+                # print(f"co_activation.shape={co_activation.shape}")
+                # each batch has a single weight change but multiple co-activations, average across the batch to obtain a batch specific co-activation
+                co_activation = np.mean(co_activation, axis=1)
+                # print(f"np.mean(co_activation, axis=1).shape={co_activation.shape}")
+                co_activations_flatten.append(co_activation)
+                pairIDs.append(
+                    [selected_channel_penultimate_layer[curr_fc1_feature],
+                     selected_channel_last_layer[curr_fc2_feature]])
+        return co_activations_flatten, weight_changes_flatten, pairIDs
+
+    co_activations_flatten_, weight_changes_flatten_, pairIDs_ = dataPrepare()
+    # mkdir(f'{directory_path}/temp')
+    if not os.path.exists(f'{directory_path}/temp'):
+        os.mkdir(f'{directory_path}/temp')
+
+    if not testMode:
+        np.save(f'{directory_path}/temp/co_activations_flatten_.npy',
+                co_activations_flatten_)  # shape = [pair#, batch#]
+        np.save(f'{directory_path}/temp/weight_changes_flatten_.npy',
+                weight_changes_flatten_)  # shape = [pair#, batch#]
+        np.save(f'{directory_path}/temp/pairIDs_.npy',
+                pairIDs_)  # shape = [pair#, [ID1, ID2]]
+
+    # co_activations_flatten_ = np.load(f'{directory_path}/temp/co_activations_flatten_.npy',
+    #                                   allow_pickle=True)  # shape = [pair#, batch#]
+    # weight_changes_flatten_ = np.load(f'{directory_path}/temp/weight_changes_flatten_.npy',
+    #                                   allow_pickle=True)  # shape = [pair#, batch#]
+    # pairIDs_ = np.load(f'{directory_path}/temp/pairIDs_.npy',
+    #                    allow_pickle=True)  # shape = [pair#, [ID1, ID2]]
+
+    def cubic_fit_correlation_with_params(x, y, n_splits=10, random_state=42, return_subset=True):
+        def cubic_function(_x, a, b, c, d):
+            return a * _x ** 3 + b * _x ** 2 + c * _x + d
+
+        # Function to compute correlation coefficient
+        def compute_correlation(observed, predicted):
+            return pearsonr(observed, predicted)[0]
+
+        # Set random seed for reproducibility
+        np.random.seed(random_state)
+
+        # Shuffle indices for k-fold cross-validation
+        indices = np.arange(len(x))
+        np.random.shuffle(indices)
+
+        # Initialize arrays to store correlation coefficients and parameters
+        correlation_coefficients = []
+        fitted_params = []
+
+        for curr_split in range(n_splits):
+            # Split data into training and testing sets
+            split_size = len(x) // n_splits
+            test_indices = indices[curr_split * split_size: (curr_split + 1) * split_size]
+            train_indices = np.concatenate([indices[:curr_split * split_size], indices[(curr_split + 1) * split_size:]])
+
+            x_train, x_test = x[train_indices], x[test_indices]
+            y_train, y_test = y[train_indices], y[test_indices]
+
+            # Perform constrained cubic fit on the training data
+            params, _ = curve_fit(cubic_function, x_train, y_train)
+
+            # Predict y values on the test data
+            y_pred = cubic_function(x_test, *params)
+
+            # Compute correlation coefficient and store it
+            correlation_coefficient = compute_correlation(y_test, y_pred)
+            correlation_coefficients.append(correlation_coefficient)
+
+            # Store fitted parameters
+            fitted_params.append(params)
+
+        # Average correlation coefficients and parameters across folds
+        mean_correlation = np.mean(correlation_coefficients)
+        mean_params = np.mean(fitted_params, axis=0)
+
+        if return_subset:
+            # Randomly choose 9% of the data for future visualization
+            subset_size = 10  # int(0.09 * len(x))
+            subset_indices = random.sample(range(len(x)), subset_size)
+            return mean_correlation, mean_params, x[subset_indices], y[subset_indices]
+        else:
+            return mean_correlation, mean_params
+
+    def run_NMPH(co_activations_flatten, weight_changes_flatten, pairIDs, rows=None, cols=None, plotFig=False):
+        if plotFig:
+            if rows is None:
+                rows = int(np.ceil(np.sqrt(len(co_activations_flatten))))
+            if cols is None:
+                cols = int(np.sqrt(len(co_activations_flatten)))
+
+            fig, axs = plt.subplots(rows, cols, figsize=(15, 15))  # Create a subplot matrix
+            from matplotlib.cm import get_cmap
+            cmap = get_cmap('viridis')  # Choose a colormap (you can change 'viridis' to your preferred one)
+        else:
+            axs = None
+            cmap = None
+
+        mean_correlation_coefficients = []
+        # recorded_data = []  # Store recorded data for visualization
+        mean_parameters = []
+        x_partials = []
+        y_partials = []
+        for i in tqdm(range(len(co_activations_flatten))):
+            if testMode:
+                testBatchNum = 50
+                x__ = co_activations_flatten[i][:testBatchNum]
+                y__ = weight_changes_flatten[i][:testBatchNum]
+                pairID = pairIDs[i]
+            else:
+                x__ = co_activations_flatten[i]
+                y__ = weight_changes_flatten[i]
+                pairID = pairIDs[i]
+            mean_correlation_coefficient, mean_parameter, x_partial, y_partial = cubic_fit_correlation_with_params(
+                x__, y__,
+                n_splits=10,
+                random_state=42,
+                return_subset=True
+            )
+            mean_correlation_coefficients.append(mean_correlation_coefficient)
+            mean_parameters.append(mean_parameter)
+            x_partials.append(x_partial)
+            y_partials.append(y_partial)
+
+            if plotFig:
+                row = i // cols
+                col = i % cols
+
+                ax = axs[row, col]  # Select the appropriate subplot
+
+                # Color the dots based on a sequence
+                sequence = np.linspace(0, 1, len(x__))  # Create a sequence of values from 0 to 1
+                colors = cmap(sequence)  # Map the sequence to colors using the chosen colormap
+
+                ax.scatter(x__, y__, s=10, c=colors)  # 's' controls the size of the points, 'c' sets the colors
+
+                # Add labels and a title to each subplot
+                ax.set_title(f'pairID: {pairID}')
+
+                # Hide x and y-axis ticks and tick labels
+                ax.set_xticks([])
+                ax.set_yticks([])
+
+        if plotFig:
+            plt.tight_layout()  # Adjust subplot layout for better visualization
+            plt.subplots_adjust(wspace=0, hspace=0)
+            plt.show()
+
+        mean_correlation_coefficients = np.array(mean_correlation_coefficients)
+        p_value = np.nanmean(mean_correlation_coefficients < 0)
+        print(f"p value = {p_value}")
+
+        # Return mean_correlation_coefficients along with recorded_data
+        return mean_correlation_coefficients, np.array(mean_parameters), np.array(x_partials), np.array(y_partials)
+
+    if testMode:
+        mean_correlation_coefficients_, mean_parameters_, x_partials_, y_partials_ = run_NMPH(
+            co_activations_flatten_[:4], weight_changes_flatten_[:4], pairIDs_[:4])
+    else:
+        mean_correlation_coefficients_, mean_parameters_, x_partials_, y_partials_ = run_NMPH(
+            co_activations_flatten_, weight_changes_flatten_, pairIDs_)
+
+    if not testMode:
+        np.save(f'{directory_path}/temp/mean_correlation_coefficients_.npy', mean_correlation_coefficients_)
+        np.save(f'{directory_path}/temp/mean_parameters_.npy', mean_parameters_)
+        np.save(f'{directory_path}/temp/x_partials_.npy', x_partials_)
+        np.save(f'{directory_path}/temp/y_partials_.npy', y_partials_)
+
+    x_partials_ = x_partials_.flatten()
+    y_partials_ = y_partials_.flatten()
+    mean_parameters_avg = np.mean(mean_parameters_, axis=0)
+
+    def plot_scatter_and_cubic(x_partials, y_partials, mean_parameters):
+        def cubic_function(_x, a, b, c, d):
+            print(f"a={a}, b={b}, c={c}, d={d}")
+            return a * _x ** 3 + b * _x ** 2 + c * _x + d
+
+        # Scatter plot
+        plt.scatter(x_partials, y_partials, label='Data Points', color='green', marker='o', s=30)
+
+        # Fit cubic curve using curve_fit
+        # popt, _ = curve_fit(cubic_function, x_partials_, y_partials_)
+
+        # Generate points for the fitted cubic curve
+        x_fit = np.linspace(min(x_partials), max(x_partials), 100)
+        y_fit = cubic_function(x_fit, *mean_parameters)
+
+        # Plot the fitted cubic curve
+        plt.plot(x_fit, y_fit, label='Fitted Cubic Curve', color='red')
+
+        # Add labels and a legend
+        plt.xlabel('X Partials')
+        plt.ylabel('Y Partials')
+        plt.legend()
+
+        # Show the plot
+        plt.show()
+
+    plot_scatter_and_cubic(x_partials_, y_partials_, mean_parameters_avg)
+
+
+def representational_level():
+    pass
 
